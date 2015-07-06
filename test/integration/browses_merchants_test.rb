e4d393df74182b63da2646c06c5ac68799a5caea
Jack Yeh <jack.yeh@gmail.com>
2015-07-06 10:32:42 -0600

    add test:can view specific item for merchant

--- test/integration/browses_merchants_test.rb
+++ test/integration/browses_merchants_test.rb
@@ -8,12 +8,25 @@ class BrowsesMerchantsTest < ActionDispatch::IntegrationTest
 
   def test_it_views_a_merchants_store_page
     merchant = Merchant.create(name: "CoolersRUs")
-    visit "/merchants/#{merchant.id}"
+    visit merchant_path(merchant)
     assert_equal 200, page.status_code
 
     within('h1') do
-      assert page.has_content?('CoolersRUs'), "page should have an CoolersRUs"
+      assert page.has_content?(merchant.name), "page should have an CoolersRUs"
     end
+  end
+
+  def test_it_views_a_specific_item
+    merchant = Merchant.create(name: "CoolersRUs")
+    item = Item.create(name: 'new item', description: "cool item", merchant_id: merchant.id)
+    visit merchant_path(merchant)
+
+    within('li') do
+      assert page.has_content?(item.name)
+      assert page.has_content?(item.description)
+      refute page.has_content?(item.merchant_id)
+    end
+
   end
 end
 
